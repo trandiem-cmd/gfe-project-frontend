6928ef1286e4c10acc96acfc3f74cc64bab6da41
Diem Tran <emma.trandiem@gmail.com>
2026-04-26 04:11:15 +0300

    add backend live URL

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 //export const BACKEND_URL = 'http://localhost:3001' ;  
-export const BACKEND_URL = 'https://gfe-project-backend-1.onrender.com' ;  
+export const BACKEND_URL = 'https://gfe-project-backend-2.onrender.com' ;  
